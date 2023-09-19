de7f2af0660306019322d7b1804a1616f2889280
Peter Hoeg <peter@hoeg.com>
2023-09-19 11:14:14 +0200

    dockerfile-ts-mode

--- modules/tools/docker/config.el
+++ modules/tools/docker/config.el
@@ -1,8 +1,26 @@
 ;;; tools/docker/config.el -*- lexical-binding: t; -*-
 
-(after! dockerfile-mode
+(use-package dockerfile-mode
+  :if (not (and (fboundp 'treesit-available-p)
+                (treesit-available-p)
+                (modulep! +tree-sitter)))
+  :defer t)
+
+(use-package dockerfile-ts-mode
+  :if (and (fboundp 'treesit-available-p)
+           (treesit-available-p)
+           (modulep! +tree-sitter))
+  :mode
+  "\\(?:Dockerfile\\(?:\\..*\\)?\\|\\.[Dd]ockerfile\\|Containerfile\\)\\'"
+  :defer t)
+
+(use-package docker :defer t)
+
+(after! (:or dockerfile-mode dockerfile-ts-mode)
   (set-docsets! 'dockerfile-mode "Docker")
-  (set-formatter! 'dockfmt '("dockfmt" "fmt" filepath) :modes '(dockerfile-mode))
+  (set-docsets! 'dockerfile-ts-mode "Docker")
+  (set-formatter! 'dockfmt '("dockfmt" "fmt" filepath) :modes '(dockerfile-mode dockerfile-ts-mode))
 
   (when (modulep! +lsp)
-    (add-hook 'dockerfile-mode-local-vars-hook #'lsp! 'append)))
+    (add-hook 'dockerfile-mode-local-vars-hook #'lsp! 'append)
+    (add-hook 'dockerfile-ts-mode-local-vars-hook #'lsp! 'append)))
--- modules/tools/docker/packages.el
+++ modules/tools/docker/packages.el
@@ -2,7 +2,11 @@
 ;;; tools/docker/packages.el
 
 (package! docker :pin "d5255a65b7240d0038cc417f301b43df05a27922")
+
 ;; tramp-container (included with Emacs 29+) replaces docker-tramp
-(when (< emacs-major-version 29)
-  (package! docker-tramp :pin "19d0771db4e6b89e19c00af5806438e315779c15"))
-(package! dockerfile-mode :pin "39a012a27fcf6fb629c447d13b6974baf906714c")
+(if (and (>= emacs-major-version 29)
+         (modulep! +tree-sitter))
+    (package! dockerfile-ts-mode :built-in t)
+  ;; tramp-container (included with Emacs 29+) replaces docker-tramp
+  (package! docker-tramp :pin "19d0771db4e6b89e19c00af5806438e315779c15")
+  (package! dockerfile-mode :pin "39a012a27fcf6fb629c447d13b6974baf906714c"))

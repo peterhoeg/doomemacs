9e3f2d905eeb33e5eb206881851c7a038aa126c1
Peter Hoeg <peter@hoeg.com>
2024-07-02 13:22:43 +0200

    docker: use tree-sitter on emacs 29

--- modules/tools/docker/config.el
+++ modules/tools/docker/config.el
@@ -1,8 +1,19 @@
 ;;; tools/docker/config.el -*- lexical-binding: t; -*-
 
-(after! dockerfile-mode
+(use-package docker :defer t)
+
+(use-package! dockerfile-mode :defer t)
+
+(use-package! dockerfile-ts-mode
+  :mode
+  "\\(?:Dockerfile\\(?:\\..*\\)?\\|\\.[Dd]ockerfile\\|Containerfile\\)\\'"
+  :defer t)
+
+(after! (:or dockerfile-mode dockerfile-ts-mode)
   (set-docsets! 'dockerfile-mode "Docker")
-  (set-formatter! 'dockfmt '("dockfmt" "fmt" filepath) :modes '(dockerfile-mode))
+  (set-docsets! 'dockerfile-ts-mode "Docker")
+  (set-formatter! 'dockfmt '("dockfmt" "fmt" filepath) :modes '(dockerfile-mode dockerfile-ts-mode))
 
   (when (modulep! +lsp)
-    (add-hook 'dockerfile-mode-local-vars-hook #'lsp! 'append)))
+    (add-hook 'dockerfile-mode-local-vars-hook #'lsp! 'append)
+    (add-hook 'dockerfile-ts-mode-local-vars-hook #'lsp! 'append)))
--- modules/tools/docker/packages.el
+++ modules/tools/docker/packages.el
@@ -2,7 +2,18 @@
 ;;; tools/docker/packages.el
 
 (package! docker :pin "6f8bba0d11a5143872dfc25afdabe16cae410d11")
+
 ;; tramp-container (included with Emacs 29+) replaces docker-tramp
-(when (< emacs-major-version 29)
-  (package! docker-tramp :pin "19d0771db4e6b89e19c00af5806438e315779c15"))
-(package! dockerfile-mode :pin "4d893bd2da15833ce056332e6c972d5d93e78f04")
+(package! docker-tramp
+  :pin "19d0771db4e6b89e19c00af5806438e315779c15"
+  :disable (>= emacs-major-version 29))
+
+(package! dockerfile-mode
+  :pin "4d893bd2da15833ce056332e6c972d5d93e78f04"
+  :disable (and (>= emacs-major-version 29)
+                (modulep! +tree-sitter)))
+
+(package! dockerfile-ts-mode
+  :built-in t
+  :disable (not (and (>= emacs-major-version 29)
+                     (modulep! +tree-sitter))))

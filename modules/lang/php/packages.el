4de8ac2acbac64846b6ff2044105af7883106ecd
Peter Hoeg <peter@hoeg.com>
2024-08-27 13:26:48 +0200

    php: php-ts-mode

--- modules/lang/php/packages.el
+++ modules/lang/php/packages.el
@@ -5,7 +5,15 @@
 (package! php-extras
   :recipe (:host github :repo "arnested/php-extras")
   :pin "d410c5af663c30c01d461ac476d1cbfbacb49367")
-(package! php-mode :pin "59814bd80c59894022bc5950fb3bdf02420e8a89")
+(if (modulep! +tree-sitter)
+    (pcase emacs-major-version
+      (28
+       (package! php-mode :pin "59814bd80c59894022bc5950fb3bdf02420e8a89"))
+      (29
+       (package! php-ts-mode
+         :pin "4e266dc4462b836b90437dc7a4d3c8f4dac89c4a"
+         :recipe (:host github :repo "emacs-php/php-ts-mode"))))
+  (package! php-mode :pin "59814bd80c59894022bc5950fb3bdf02420e8a89"))
 (package! php-refactor-mode :pin "7a794b0618df2882b1bd586fdd698dba0bc5130d")
 (package! phpunit :pin "650a50898de1fa4eeb47360b12aeb1126b2448c2")
 (package! composer :pin "791a7104be2ef2748757a186094c1e8f7f531a01")

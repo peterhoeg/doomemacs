fcb48100f19cfcd16aa36c3eb031bcbcdc14437a
Henrik Lissner <git@henrik.io>
2026-07-10 15:51:55 -0400

    fix: package!: avoid map-delete
    There's no guarantee map.el will be loaded by now, and there's a much
    simpler alternative: cl-remf.
    
    Fix: #8849

--- lisp/doom-packages.el
+++ lisp/doom-packages.el
@@ -996,7 +996,7 @@ elsewhere."
     (when (and (not ignore)
                (equal built-in '(quote prefer)))
       (setq built-in `(locate-library ,(symbol-name name) nil (get 'load-path 'initial-value))))
-    (cl-callf map-delete plist :built-in)
+    (cl-remf plist :built-in)
     (cl-callf plist-put plist :ignore built-in))
   `(let* ((name ',name)
           (plist (cdr (assq name doom-packages)))

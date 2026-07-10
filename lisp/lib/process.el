peterhoeg/doomemacs

commit b4c2f89cce102eaaaf42488d603f3c38ecf36d72

fix(lib): doom-{call,exec}-process: only strip final newline
Instead of trimming all whitespace on both sides, which may mangle
desired output.

lisp/lib/process.el
@@ -1,5 +1,11 @@
 ;;; lisp/lib/process.el -*- lexical-binding: t; -*-
 
+(defun doom--strip-final-newline ()
+  (save-excursion
+    (goto-char (point-max))
+    (while (looking-at-p "^$")
+      (delete-char -1))))
+
 ;;;###autoload
 (defun doom-call-process (command &rest args)
   "Execute COMMAND with ARGS synchronously.
@@ -9,7 +15,9 @@ code of the process and OUTPUT is its stdout output."
   (with-temp-buffer
     (cons (or (apply #'call-process command nil t nil (remq nil args))
               -1)
-          (string-trim (buffer-string)))))
+          (progn
+            (doom--strip-final-newline)
+            (buffer-string)))))
 
 ;;;###autoload
 (defun doom-exec-process (command &rest args)
@@ -39,7 +47,9 @@ Warning: freezes indefinitely on any stdin prompt."
             (while (not done-p)
               (sit-for 0.1))
             (process-exit-status process))
-          (string-trim (buffer-string)))))
+          (progn
+            (doom--strip-final-newline)
+            (buffer-string)))))
 
 (provide 'doom-lib '(process))
 ;;; process.el ends here

commit b5814087bd09467f76269214494a8e254e4255fd

fix(lib): doom-{exec,call}-process regression
- Fixes a regression introduced in b4c2f89: this would throw a
  beginning-of-buffer error if the process produced no output.
- b4c2f89 was supposed to stop these functions from trimming *all*
  surrounding whitespace, and succeeded for leading whitespace, but
  still consumes all trailing whitespace (it should only delete a single
  newline). This fixes that.

Amend: b4c2f89cce10

lisp/lib/process.el
@@ -1,10 +1,11 @@
 ;;; lisp/lib/process.el -*- lexical-binding: t; -*-
 
 (defun doom--strip-final-newline ()
-  (save-excursion
-    (goto-char (point-max))
-    (while (looking-at-p "^$")
-      (delete-char -1))))
+  (unless (= (point-min) (point-max))
+    (save-excursion
+      (goto-char (point-max))
+      (when (looking-at-p "^$")
+        (delete-char -1)))))
 
 ;;;###autoload
 (defun doom-call-process (command &rest args)
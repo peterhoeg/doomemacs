0e279b5d47cf8bdaa2c2b0edaca27324301421fb
Henrik Lissner <git@henrik.io>
2026-08-29 15:01:31 -0400

    fix: recentf: bump mru list on buffer focus
    I put this off while investigating concerns with
    `window-buffer-change-functions`, which turned out to be a non-issue, so
    now I'm restoring this (after it was undone by c080b3f).
    
    Amend: c080b3f3adb3

--- lisp/doom-emacs.el
+++ lisp/doom-emacs.el
@@ -1654,7 +1654,8 @@ with `set-indent-vars!'."
 
   ;; UX: Reorder the recent files list by frecency (i.e. every time you touch a
   ;;   buffer, bump it to the top of the list).
-  (add-hook 'doom-switch-window-hook #'recentf-track-opened-file)
+  (add-hook! '(doom-switch-buffer-hook doom-switch-window-hook)
+             #'recentf-track-opened-file)
   (add-hook! 'dired-mode-hook
     (defun doom--recentf-add-dired-directory-h ()
       "Add dired directories to recentf file list."
